f1c1dee7b945e1aa79efde5ae565643e3231e41a
Viktoriya <v.matveyeva@yandex.ru>
2020-10-05 23:01:55 +0400

    benefits

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,37 @@
 $(document).ready(function() {
+    const benefitsSlider = $('.benefiits-block')
+	benefitsSlider.owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: true,
+		autoplaySpeed: 1000,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			460: {
+				items: 2,
 
+			},
+			768: {
+				items: 3,
+
+			},
+			992: {
+				items: 4,
+
+			}
+		}
+
+
+	});
+	if ($(window).width() > 1200) {
+		benefitsSlider.trigger('destroy.owl.carousel');
+	}
+	$(window).resize(function () {
+		if ($(window).width() > 1200) {
+			benefitsSlider.trigger('destroy.owl.carousel');
+		}
+	});
 
 })
