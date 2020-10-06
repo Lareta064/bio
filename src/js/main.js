e5da889a93276fe2ad3503206cd5c6bc4751fc10
Lareta <lareta064@gmail.com>
2020-10-06 18:35:53 +0300

    правки скрипта меню

--- src/js/main.js
+++ src/js/main.js
@@ -44,6 +44,7 @@ $(document).ready(function () {
 	const logoBlock = document.querySelector('.logo-block');
 	const headerSearchForm = document.querySelector('#search-form');
 	const searchResultBlock = document.querySelector('#search-result');
+	const searchIcons = document.querySelectorAll('#search-button svg');
 
 	menuToggle.addEventListener('click', function () {
 		if (this.classList.contains('active')) {
@@ -52,11 +53,23 @@ $(document).ready(function () {
 			overlayBlock.classList.remove('active');
 			bodyEl.classList.remove('noscroll');
 
+
 		} else {
 			this.classList.add('active');
 			mobMenu.classList.add('active');
 			overlayBlock.classList.add('active');
 			bodyEl.classList.add('noscroll');
+
+			// скрыть меню поиска при клике по гамбургеру
+			headerSearchForm.classList.remove('visible');
+			searchResultBlock.classList.remove('visible');
+			logoBlock.classList.remove('noborder');
+			logoBlock.classList.remove('hidden');
+			for (let i = 0; i < searchIcons.length; i++) {
+				searchIcons[0].classList.add('visible');
+				searchIcons[1].classList.remove('visible');
+			}
+
 
 		}
 	});
@@ -64,16 +77,13 @@ $(document).ready(function () {
 	// показать строку поиска в шапке
 	btnShowSearchInput.addEventListener('click', function (e) {
 		e.preventDefault();
-		const thisIcons = this.querySelectorAll('#search-button svg');
+
 		if (headerSearchForm.classList.contains('visible')) {
 			headerSearchForm.classList.remove('visible');
 			logoBlock.classList.remove('noborder');
 			bodyEl.classList.remove('noscroll');
 			searchResultBlock.classList.remove('visible');
-			if (window.innerWidth < 768) {
-				logoBlock.classList.remove('hidden');
-			}
-
+			logoBlock.classList.remove('hidden');
 
 		} else {
 			headerSearchForm.classList.add('visible');
@@ -86,12 +96,10 @@ $(document).ready(function () {
 				mobMenu.classList.remove('active');
 				menuToggle.classList.remove('active');
 				overlayBlock.classList.remove('active');
-
-
 			}
 
 		}
-		for (item of thisIcons) {
+		for (item of searchIcons) {
 			item.classList.toggle('visible');
 		}
 	});
