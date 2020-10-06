9d850f7093b5383e77e81b799ff4603ee27450bd
Lareta <lareta064@gmail.com>
2020-10-06 16:51:21 +0300

    header-v2

--- src/js/main.js
+++ src/js/main.js
@@ -35,39 +35,15 @@ $(document).ready(function () {
 		}
 	});
 
-
-
-	// показать строку поиска в шапке
+	// Открытие моб меню по клику на гамбургер
+	const menuToggle = document.querySelector('.menu-toggle');
+	const mobMenu = document.querySelector('#mobile-menu');
 	const bodyEl = document.body;
 	const overlayBlock = document.querySelector('#overlay');
 	const btnShowSearchInput = document.querySelector('#search-button');
 	const logoBlock = document.querySelector('.logo-block');
 	const headerSearchForm = document.querySelector('#search-form');
 	const searchResultBlock = document.querySelector('#search-result');
-	btnShowSearchInput.addEventListener('click', function (e) {
-		e.preventDefault();
-		const thisIcons = this.querySelectorAll('#search-button svg');
-		if (headerSearchForm.classList.contains('visible')) {
-			headerSearchForm.classList.remove('visible');
-			logoBlock.classList.remove('noborder');
-			bodyEl.classList.remove('noscroll');
-			searchResultBlock.classList.remove('visible');
-
-
-		} else {
-			headerSearchForm.classList.add('visible');
-			logoBlock.classList.add('noborder');
-			searchResultBlock.classList.add('visible');
-			bodyEl.classList.add('noscroll');
-
-		}
-		for (item of thisIcons) {
-			item.classList.toggle('visible');
-		}
-	});
-	// Открытие моб меню по клику на гамбургер
-	const menuToggle = document.querySelector('.menu-toggle');
-	const mobMenu = document.querySelector('#mobile-menu');
 
 	menuToggle.addEventListener('click', function () {
 		if (this.classList.contains('active')) {
@@ -84,4 +60,40 @@ $(document).ready(function () {
 
 		}
 	});
+
+	// показать строку поиска в шапке
+	btnShowSearchInput.addEventListener('click', function (e) {
+		e.preventDefault();
+		const thisIcons = this.querySelectorAll('#search-button svg');
+		if (headerSearchForm.classList.contains('visible')) {
+			headerSearchForm.classList.remove('visible');
+			logoBlock.classList.remove('noborder');
+			bodyEl.classList.remove('noscroll');
+			searchResultBlock.classList.remove('visible');
+			if (window.innerWidth < 768) {
+				logoBlock.classList.remove('hidden');
+			}
+
+
+		} else {
+			headerSearchForm.classList.add('visible');
+			logoBlock.classList.add('noborder');
+			searchResultBlock.classList.add('visible');
+			bodyEl.classList.add('noscroll');
+
+			if (window.innerWidth < 768) {
+				logoBlock.classList.add('hidden');
+				mobMenu.classList.remove('active');
+				menuToggle.classList.remove('active');
+				overlayBlock.classList.remove('active');
+
+
+			}
+
+		}
+		for (item of thisIcons) {
+			item.classList.toggle('visible');
+		}
+	});
+
 })
