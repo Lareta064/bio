7fc9021f11c07c6a876664e6a21077dfd2470e7d
Lareta <lareta064@gmail.com>
2020-10-19 16:14:00 +0300

    new

--- src/js/main.js
+++ src/js/main.js
@@ -37,25 +37,16 @@ $(window).resize(function () {
 	}
 });
 
-// $('.top-slide .item img').each(function () {
-// 		let ico = $(this).attr('src');
-// 		$('.after-slide').append('<span class="img" style="background: url(' + ico + ')"></span>');
-// 	});
-// 	$('.top-slide').owlCarousel({
-// 		items: 1,
-// 		autoplay: true,
-// 		autoplayTimeout: 6000,
-// 		nav: false,
-// 		dots: true,
-// 		loop: true,
-// 		animateOut: 'fadeOut',
-// 		dotsContainer: '.after-slide',/*Указываем класс блока пагинации*/
-// 		autoplayHoverPause: true
-// 	});
-// 	$('.after-slide').appendTo('.top-slide');
-// 	$('.after-slide img').click(function () {
-// 		$('.top-slide').trigger('to.owl.carousel', [$(this).index(), 300]);
-// 	});
+
+	$('.product-page__slider').owlCarousel({
+		items: 1,
+		autoplay: true,
+		autoplaySpeed: 2500,
+		nav: false,		
+		loop: true,			
+		autoplayHoverPause: true
+	});
+
 
 // HEADER SLIDER
 let mainSlider = $('.header-slider__wrapper');
